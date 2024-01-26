b5caed25d4e98ddfb2423d5f12624b16334d3a05
sumayaa4406037 <149380178+sumayaa4406037@users.noreply.github.com>
2024-01-26 22:07:15 +0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,54 @@
+// var sudentArray=['Akhy','Asifa','Trina','Rimi','Tuli','Mohona','Saima','Jerin'];
+// console.log(sudentArray[0]);
+// console.log(sudentArray[1]);
+// console.log(sudentArray[2]);
+// console.log(sudentArray[3]);
+// console.log(sudentArray[4]);
+// console.log(sudentArray[5]);
+// console.log(sudentArray[6]);
+// console.log(sudentArray[7]);
+
+
+// var sudentArray=['Akhy','Asifa','Trina','Rimi','Tuli','Mohona','Saima','Jerin'];
+// var i=0;
+// while(i<=7){
+//     console.log(sudentArray[i])
+//     i++;
+// }
+
+
+// for(var i=1;i<=100;i++){
+//     console.log(i);
+// }
+
+
+// for(var i=2;i<=100;i=i+2){
+//     console.log(i);
+// }
+
+
+// var sudentArray=['Akhy','Asifa','Trina','Rimi','Tuli','Mohona','Saima','Jerin'];
+//     for(var i=0;i<=7;i++){
+//         console.log(sudentArray[i]+'-'+i);
+// }
+
+
+// var x=32;
+// while(x<=100){
+//     console.log(x);
+//     x=x+2;
+// }
+
+
+// var i=1;
+// do{
+//  console.log(i);
+//  i++;
+// }while(i<=100)
+
+
+// var total=0;
+// for (var i=2;i<=10;i=i+2){
+//     total=total+i;
+// }
+// console.log(total);
